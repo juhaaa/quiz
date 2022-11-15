e33ea6f7e8cf347e17d79fbb9384c80fe83223dd
Juha Leikas <juha.leikas@gmail.com>
2022-11-15 17:02:32 +0200

    unicafe- tests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -10,3 +10,20 @@ class TestMaksukortti(unittest.TestCase):
     
     def test_saldo_oikein_alussa(self):
         self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataus(self):
+        self.maksukortti.lataa_rahaa(100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 11.00 euroa")
+
+    def test_ota_rahaa(self):
+        self.maksukortti.ota_rahaa(100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 9.00 euroa")
+        
+    def test_kortilla_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(1100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+
