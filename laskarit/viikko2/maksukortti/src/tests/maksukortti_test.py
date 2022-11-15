62fd1c99f5f7fc0c1641482132e8352e3c78e8b6
Juha Leikas <juha.leikas@gmail.com>
2022-11-15 15:10:28 +0200

    maksukortti test

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,9 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
