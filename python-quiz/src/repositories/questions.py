228d9c65471ddb0e0928f8fdab0806df0ab76ee6
Juha Leikas <juha.leikas@gmail.com>
2022-11-29 19:10:44 +0200

    new db, rewrite questions, new tests

--- python-quiz/src/repositories/questions.py
+++ python-quiz/src/repositories/questions.py
@@ -12,11 +12,41 @@ class Questions:
         self._cur = self._db.cursor()
         
         
-    #Returns specific amount of tuples, after randomizing the order. Tuples contain target country, correct and false answers.
+    #Returns tuple of correct answer, country name and list of answers , after randomizing the order. 
+    #Tuples contain target country, correct and false answers.
     #DB under progress.
 
     def get_questions(self):
-        
+
         data = self._cur.execute("SELECT * FROM Countries ORDER BY RANDOM() LIMIT 1").fetchall()
-        return data
+        cca2 = data[0][0]
+        cname = data[0][1]
+        correct = data[0][2]
+        if correct == "":
+            correct = "No capital city"
+
+        answers = self.get_wrong_answers(cca2)
+        answers.append(correct)
+        random.shuffle(answers)
+
+
+        return correct, cname, answers
+
+    #Gets answers for question. Answers usually cities within the same country.
+    #If not possible, gathers answers from other countries.
+
+    def get_wrong_answers(self, cca2):
+
+        data = self._cur.execute("SELECT Cities.name FROM Cities, Countries WHERE Cities.cca2=? AND Cities.cca2=Countries.cca2 AND Countries.capital<>Cities.name ORDER BY RANDOM() LIMIT 3",[cca2]).fetchall()
+        answer_count = len(data)
+
+        if answer_count < 3:
+            amount = 3- answer_count
+            extra_answers = self._cur.execute("SELECT Cities.name FROM Cities, Countries WHERE Countries.cca2=Cities.cca2 AND Countries.capital<>Cities.name ORDER BY RANDOM() LIMIT ?",[amount]).fetchall()
+            for i in extra_answers:
+                data.append(i)
+
+        return [data[0][0], data[1][0], data[2][0]]
+
+
 
--- python-quiz/src/tests/questions_test.py
+++ python-quiz/src/tests/questions_test.py
@@ -6,17 +6,29 @@ from connection import get_db_connection
 class TestQuestions(unittest.TestCase):
     def setUp(self):
         self.a = Questions(get_db_connection())
-        self.questions = self.a.get_questions(2)
+        self.questions = self.a.get_questions()
 
-    #Test for set amount.
+    #Test for correct answer in answers.
 
-    def test_amount(self):
-        self.assertEqual(self.a.amount, 2)
+    def test_correct_in_answers(self):
+        correct = self.questions[0]
+        answers = self.questions[2]
+        assert correct in answers
 
-    #Test for returned amount.
+    #Test for returned amount of answers.
 
-    def test_amount_questions(self):
-        self.lenght = len(self.questions)
-        self.assertEqual(self.lenght, 2)
+    def test_amount_answers(self):
+        self.answers_amount = len(self.questions[2])
+        self.assertEqual(self.answers_amount, 4)
+
+    #Test in case no capital city exist
+    
+    def test_for_no_capital(self):
+        while self.questions[0] != "No capital city":
+            self.questions = self.a.get_questions()
+        answers = self.questions[2]
+
+        assert "No capital city" in answers
+
 
     
